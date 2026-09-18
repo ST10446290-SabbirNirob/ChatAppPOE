9149c9864af5906e14a30722061518e1376730a4
ST10446290-SabbirNirob <ST10446290@vcconnect.edu.za>
2026-09-18 18:47:59 +0200

    Created the Login class
    Added the login class in the file

---
+++ src/chatapppoe/Login.java
@@ -0,0 +1,19 @@
+//******************************************************************************
+//                                   START                                      
+//******************************************************************************
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatapppoe;
+
+/**
+ *
+ * @author Sabbir Nirob ST10446290 sabbirnirob1000@gmail.com
+ */
+
+//******************************************************************************
+
+public class Login {
+    
+}
