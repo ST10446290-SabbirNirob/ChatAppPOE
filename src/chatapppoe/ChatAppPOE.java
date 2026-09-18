f6b1639994a5eeb5dedad2b6447f939dfe286d8d
ST10446290-SabbirNirob <ST10446290@vcconnect.edu.za>
2026-09-18 18:28:14 +0200

    Initial commit

---
+++ src/chatapppoe/ChatAppPOE.java
@@ -0,0 +1,27 @@
+//******************************************************************************
+//                                   START                                      
+//******************************************************************************
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatapppoe;
+
+/**
+ *
+ * @author Sabbir Nirob ST10446290 sabbirnirob1000@gmail.com
+ */
+
+//******************************************************************************
+
+public class ChatAppPOE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Chat App POE started successfully!");
+    }
+    
+}
